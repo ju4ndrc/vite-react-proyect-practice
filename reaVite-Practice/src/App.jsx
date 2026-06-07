2c52a0dce7172d868cfca25da8acf5724d3c9f61
ju4ndrc <jdramirez90@ucatolica.edu.co>
2026-06-06 23:18:15 -0500

    [+]Imports fixed

--- reaVite-Practice/src/App.jsx
+++ reaVite-Practice/src/App.jsx
@@ -1,87 +1,19 @@
-import { use, useState } from 'react'
+
 
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
-import JobCard from './components/JobCard.jsx'
-import SearchFormSection from './components/SearchFormSection.jsx'
-import JobListings from './components/JobListings.jsx'
-import Pagination from './components/Pagination.jsx'
-import jobsData from './data.json'
-import './App.css'
 
-const RESULT_PER_PAGE = 3
+import HomePage from './components/pages/Home.jsx'
+import SearchPage from './components/pages/Seacrh.jsx'
 
 function App() {
 
-  const [filters, setFilters] = useState({
-      technology:'',
-      location: '',
-      experienceLevel: ''
-  })
-  const [textToFilter, setTextToFilter] = useState('')
-  const [currentPage, setCurrentPage] = useState(1)
 
-  const jobsFilterByFilter = jobsData.filter(job=>{
-    return (
-      ( filters.technology === '' || job.data.technology === filters.technology ) &&
-      ( filters.location === ''|| job.data.modalidad === filters.location ) &&
-      ( filters.experienceLevel === ''|| job.data.nivel === filters.experienceLevel  )
-
-  )
-  })
-  
-  const jobsWithTextFilter = textToFilter === ''
-  ?jobsFilterByFilter 
-   : jobsFilterByFilter.filter(job=>{
-    return job.titulo.toLowerCase().includes(textToFilter.toLowerCase())
-  })
-  
-  const totalPages = Math.ceil(jobsWithTextFilter.length / RESULT_PER_PAGE)
-  const handlePageChange =  (page)  =>{
-    console.log('Chanin page',page)
-    setCurrentPage(page)
-    window.scrollTo({top:0, behavior:'smooth'})    
-  }
-  
-  const pagedResults = jobsWithTextFilter.slice(
-    (currentPage-1)* RESULT_PER_PAGE,
-    currentPage * RESULT_PER_PAGE
-  )
-  
-  const handleSearch = (filters)=>{
-    setFilters(filters)
-    setCurrentPage(1)
-    
-    
-  }
-  const handleTextFilter = (newTextToFilter)=>{
-    setTextToFilter(newTextToFilter)
-    setCurrentPage(1)
-  }
-  const handleReset = ()=>{
-    setFilters=({
-      technology:'',
-      location: '',
-      experienceLevel: ''
-    })
-    setTextToFilter('')
-    setCurrentPage(1)
-  }
   return (
     <>
     <Header/>
 
-  <main>
-
-    <SearchFormSection onSearch={handleSearch} onTextFilter={handleTextFilter} onReset={handleReset}></SearchFormSection>
-    <section>
-      <JobListings jobs={pagedResults}></JobListings>
-
-      
-      <p> Mostrando {(currentPage -1) * RESULT_PER_PAGE + 1} -{' '} {Math.min(currentPage * RESULT_PER_PAGE, jobsData.length)} de {jobsData.length} Resultados</p>
-      <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange}></Pagination>
-    </section>
-  </main>
+    <HomePage/>
 
     <Footer/>
 
---
+++ reaVite-Practice/src/components/pages/Seacrh.jsx
@@ -0,0 +1,90 @@
+import { use, useState } from 'react'
+
+
+
+import SearchFormSection from '../SearchFormSection.jsx'
+import JobListings from '../JobListings.jsx'
+import Pagination from '../Pagination.jsx'
+import jobsData from '../../data.json'
+import '../../App.css'
+
+const RESULT_PER_PAGE = 3
+
+function SearchPage() {
+
+  const [filters, setFilters] = useState({
+      technology:'',
+      location: '',
+      experienceLevel: ''
+  })
+  const [textToFilter, setTextToFilter] = useState('')
+  const [currentPage, setCurrentPage] = useState(1)
+
+  const jobsFilterByFilter = jobsData.filter(job=>{
+    return (
+      ( filters.technology === '' || job.data.technology === filters.technology ) &&
+      ( filters.location === ''|| job.data.modalidad === filters.location ) &&
+      ( filters.experienceLevel === ''|| job.data.nivel === filters.experienceLevel  )
+
+  )
+  })
+  
+  const jobsWithTextFilter = textToFilter === ''
+  ?jobsFilterByFilter 
+   : jobsFilterByFilter.filter(job=>{
+    return job.titulo.toLowerCase().includes(textToFilter.toLowerCase())
+  })
+  
+  const totalPages = Math.ceil(jobsWithTextFilter.length / RESULT_PER_PAGE)
+  const handlePageChange =  (page)  =>{
+    console.log('Chanin page',page)
+    setCurrentPage(page)
+    window.scrollTo({top:0, behavior:'smooth'})    
+  }
+  
+  const pagedResults = jobsWithTextFilter.slice(
+    (currentPage-1)* RESULT_PER_PAGE,
+    currentPage * RESULT_PER_PAGE
+  )
+  
+  const handleSearch = (filters)=>{
+    setFilters(filters)
+    setCurrentPage(1)
+    
+    
+  }
+  const handleTextFilter = (newTextToFilter)=>{
+    setTextToFilter(newTextToFilter)
+    setCurrentPage(1)
+  }
+  const handleReset = ()=>{
+    setFilters=({
+      technology:'',
+      location: '',
+      experienceLevel: ''
+    })
+    setTextToFilter('')
+    setCurrentPage(1)
+  }
+  return (
+    
+    
+
+  <main>
+
+    <SearchFormSection onSearch={handleSearch} onTextFilter={handleTextFilter} onReset={handleReset}></SearchFormSection>
+    <section>
+      <JobListings jobs={pagedResults}></JobListings>
+
+      
+      <p> Mostrando {(currentPage -1) * RESULT_PER_PAGE + 1} -{' '} {Math.min(currentPage * RESULT_PER_PAGE, jobsData.length)} de {jobsData.length} Resultados</p>
+      <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange}></Pagination>
+    </section>
+  </main>
+
+
+    
+  )
+}
+
+export default SearchPage
