6d39da7bac201a2f4582e963e867976f5e151ac4
ju4ndrc <jdramirez90@ucatolica.edu.co>
2026-06-08 22:21:07 -0500

    [+]implementing error not found

---
+++ reaVite-Practice/src/components/pages/404.jsx
@@ -0,0 +1,8 @@
+export default function NotFoundPage(){
+    return(
+        <>
+        <h1>404 - Pagina no encontrada</h1>
+        <p>Lo sentimos , pagina no encontrada</p>
+        </>
+    )
+}
